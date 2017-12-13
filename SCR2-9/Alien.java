cd5fab4942c8bc4ca2284a2be6eb50637487d303
chanryu7120 <chanryu9120@gmail.com>
2017-12-13 10:26:14 +0900

    Alien B

--- SCR2-9/Alien.java
+++ SCR2-9/Alien.java
@@ -14,6 +14,13 @@ public class Alien extends Actor
      */
     public void act() 
     {
-        // Add your action code here.
+        if( Greenfoot.isKeyDown( "right" ) ){
+            setRotation(0);
+            move(1);
+        }
+        if( Greenfoot.isKeyDown( "left" ) ){
+            setRotation(180);
+            move(1);
+        }
     }    
 }
