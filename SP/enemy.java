3db80f70eb31e2fa849cbc94c3c6067d565a8a8f
yossyi <magu.pul.masada@gmail.com>
2018-02-13 16:26:34 +0900

    C

--- SP/enemy.java
+++ SP/enemy.java
@@ -13,7 +13,7 @@ public class enemy extends Actor
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     private int flag = 0;
-    private int i = 1;
+    private int i = 2;
     private int j = 0;
     public void act() 
     {
@@ -26,7 +26,7 @@ public class enemy extends Actor
                 move(0);
             }  
 
-            
+           move(400);
             setLocation(x+i,y+j);
         
             checkObstacle1();  
@@ -46,23 +46,23 @@ public class enemy extends Actor
         setLocation(x-i,y-j);
          if(rand >= 0 && rand < 90)
         {
-            i = -1;
+            i = -2;
             j = 0;
         } 
         if(rand >= 90 && rand < 180)
         {
-            i = 1;
+            i = 2;
             j = 0;
         } 
         else if(rand >= 180 && rand < 270)
         {
             i = 0;
-            j = 1;
+            j = 2;
         } 
         else if(rand >= 270 && rand < 360)
         {
             i = 0;
-            j = -1;
+            j = -2;
         } 
         
         
